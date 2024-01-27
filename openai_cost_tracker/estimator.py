9bff2c8404a4922b22b0bd475a7be33a293bcbac
ruoyuxie <royx252@gmail.com>
2024-01-26 21:17:01 -0500

    gpt-4-0125-preview update

--- openai_cost_tracker/estimator.py
+++ openai_cost_tracker/estimator.py
@@ -13,6 +13,7 @@ class CostEstimator:
         "gpt-4": {"input": 0.03, "output": 0.06},
         "gpt-3.5-turbo-1106": {"input": 0.0015, "output": 0.002},
         "gpt-4-1106-preview": {"input": 0.01, "output": 0.03},
+        "gpt-4-0125-preview": {"input": 0.01, "output": 0.03},
     }
 
     total_cost = 0.0  # class variable to persist total_cost
